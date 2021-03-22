cdf8ad6327cec19f716034ba45c131544c1a315c
Andreas Marsh <marsh.andreas.s@gmail.com>
2021-03-22 11:01:39 -0400

    Initial Commit

---
+++ JukeBox/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JukeBox
+//
+//  Created by Andreas Marsh on 3/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JukeBox/JukeBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  JukeBoxApp.swift
+//  JukeBox
+//
+//  Created by Andreas Marsh on 3/22/21.
+//
+
+import SwiftUI
+
+@main
+struct JukeBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
